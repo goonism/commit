33f18ff45586ca4491629275828023338e7e4a5d
Daniel Sinclair <d@niel.nyc>
2017-11-11 02:08:26 -0500

    basic messages

--- src/components/Messages/index.jsx
+++ src/components/Messages/index.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import './index.css';
+
+class MessageCell extends Component {
+  render() {
+    return (
+      <div className="messageCell" style={{
+        backgroundColor: "red",
+        width: "100%",
+        minHeight: "40px",
+        maxHeight: "auto",
+        margin: "10px"
+      }}>
+        <span>{this.props.text}</span> 
+      </div>
+    );
+  }
+}
 
 export default class Messages extends Component {
   render() {
     return (
-      <div/>
+      <div style={{
+        width: "100%",
+        height: "100vh"
+      }}>
+        <MessageCell text="hello"/>
+      </div>
     );
   }
 }
